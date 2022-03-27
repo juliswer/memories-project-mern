accced75c8b220f56936e43e2ad79ef06dd89035
Julian Swerdlin <julianswer36@gmail.com>
2022-03-27 01:03:17 -0300

    uploading the back

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from "express";
+import {getPosts} from '../controllers/posts.js'
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send('hello world')
-});
+router.get("/", getPosts);
 
 export default router;
