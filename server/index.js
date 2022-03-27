722c52f750e05bf51621107f62c2916403e3bb4a
Julian Swerdlin <julianswer36@gmail.com>
2022-03-27 00:53:30 -0300

    uploading the back

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from 'mongoose';
-import cors from 'cors';
+import mongoose from "mongoose";
+import cors from "cors";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log("Something went wrong whit the database", error.message);
+  });
