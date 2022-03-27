7286c767010850b34287f48e83e28f5de8b27493
Julian Swerdlin <julianswer36@gmail.com>
2022-03-27 00:58:02 -0300

    uploading the back

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 import { config } from "dotenv";
 config();
 
 const app = express();
 
+app.use("/posts", postRoutes); 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
