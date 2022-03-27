84fcd6d44ecfe9bbc64adebd0b23e10af48ea198
Julian Swerdlin <julianswer36@gmail.com>
2022-03-27 12:38:26 -0300

    updating the back

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ config();
 
 const app = express();
 
-app.use("/posts", postRoutes); 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes); 
 
 mongoose
   .connect(process.env.MONGO_URI, {
