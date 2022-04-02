8368924d3bda410c989ae79dd472f40f537463ad
Julian Swerdlin <julianswer36@gmail.com>
2022-04-02 17:46:37 -0300

    auth page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
 
 const Navbar = () => {
   const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
-    const user = null;
+  console.log(user);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" variant="h2" align="center" className={classes.heading}>
+        <Typography
+          component={Link}
+          to="/"
+          variant="h2"
+          align="center"
+          className={classes.heading}
+        >
           Memories
         </Typography>
         <img
@@ -24,13 +31,39 @@ const Navbar = () => {
       </div>
       <Toolbar className={classes.toolbar}>
         {user ? (
-            <div className={classes.profile}>
-                <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                <Typography className={classes.userName} component={Link} to="/" variant="h6">{user.result.name}</Typography>
-                <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
-            </div>
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography
+              className={classes.userName}
+              component={Link}
+              to="/"
+              variant="h6"
+            >
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+            >
+              Logout
+            </Button>
+          </div>
         ) : (
-            <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
         )}
       </Toolbar>
     </AppBar>
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,10 +1,10 @@
 import { AUTH, LOGOUT } from "../constants/actionTypes";
 
-const authReducer = (state = {authData: null}, action) => {
+const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
-      console.log(action?.data);
-      return state;
+      localStorage.setItem(`profile`, JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action?.data };
     case LOGOUT:
       break;
     default:
