d64b6f3d8fd6611e858d69e15b83b3d30293abac
Julian Swerdlin <julianswer36@gmail.com>
2022-04-02 13:24:24 -0300

    reponsive changes

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -6,6 +6,7 @@ import {
   Grid,
   Typography,
   Container,
+  TextField,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
@@ -15,13 +16,42 @@ const Auth = () => {
 
   const isSignUp = false;
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting");
+  };
+
+  const handleChange = (e) => {};
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography variant="h5">Sign Up</Typography>
+        <Typography variant="h5">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Grid xs={6} md={12}>
+                  <TextField
+                    name="firstName"
+                    label="First Name"
+                    handleChange={handleChange}
+                    autoFocus
+                  />
+                  <TextField
+                    name="firstName"
+                    label="First Name"
+                    handleChange={handleChange}
+                    autoFocus
+                  />
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </form>
       </Paper>
     </Container>
   );
---
+++ client/src/components/Auth/Input.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { TextField, Grid, InputAdorment, IconButton } from "@material-ui/core";
+
+const Input = ({ half, type, name, autoFocus, label }) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password" && {
+            endAdornment: (
+              <InputAdorment position="end">
+                <IconButton onClick={handleShowPassword}>
+                  {type === "password" ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdorment>
+            ),
+          }
+        }
+      />
+    </Grid>
+  );
+};
+
+export default Input;
