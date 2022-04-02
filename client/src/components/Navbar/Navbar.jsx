a8e299ea6440c8064d9c54db6e633ea29dbd87a3
Julian Swerdlin <julianswer36@gmail.com>
2022-04-02 13:00:59 -0300

    reponsive changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                 <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
             </div>
         ) : (
-            <Button component={Link} to="/auth" varaint="contained" color="primary">Sign In</Button>
+            <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
         )}
       </Toolbar>
     </AppBar>
