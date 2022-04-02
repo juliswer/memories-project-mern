7e3d28ea91d694467d251a4a318d15834325511e
Julian Swerdlin <julianswer36@gmail.com>
2022-04-02 13:11:04 -0300

    auth page

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import useStyles from "./styles";
 
 const Auth = () => {
+  const classes = useStyles();
 
-    const state = null;
+  const isSignUp = false;
 
   return (
-    <div>Auth</div>
-  )
-}
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">Sign Up</Typography>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Auth
+export default Auth;
