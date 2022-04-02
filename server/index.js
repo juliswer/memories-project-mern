5f46e0fc7bb8812267bab55d12d0ac91a1322177
Julian Swerdlin <julianswer36@gmail.com>
2022-04-02 11:46:48 -0300

    reponsive changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/posts", postRoutes); 
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -19,7 +21,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(process.env.PORT, () =>
+    app.listen(PORT, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
   })
