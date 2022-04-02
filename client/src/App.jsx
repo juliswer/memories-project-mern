4a28fdb7f5f6ebc666d506c05796d2e22b00d2e2
Julian Swerdlin <julianswer36@gmail.com>
2022-04-02 12:44:08 -0300

    reponsive changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
-import memories from "./images/memories.png";
+import Navbar from './components/Navbar/Navbar';
 import useStyles from "./styles";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
@@ -18,17 +18,7 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center" className={classes.heading}>
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
           <Grid
