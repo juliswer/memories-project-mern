4a8e46400da7d8384bb260f6267a4e7809718d73
Julian Swerdlin <julianswer36@gmail.com>
2022-04-03 17:14:41 -0300

    auth page

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+config();
+const JWTSecret = process.env.SECRET_JWT_KEY;
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, JWTSecret);
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
