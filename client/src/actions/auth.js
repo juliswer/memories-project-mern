2d5a34cdfbb822c00313b03b00a014b6b3a94480
store <julianswer36@gmail.com>
2022-04-07 20:22:32 -0300

    new

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,7 +4,6 @@ import * as api from "../api";
 export const signin = (formData, history) => async (dispatch) => {
   try {
     const {data} = await api.signIn(formData);
-
     dispatch({type: AUTH, data});
 
     history.push("/");
@@ -16,7 +15,7 @@ export const signin = (formData, history) => async (dispatch) => {
 export const signup = (formData, history) => async (dispatch) => {
   try {
     const {data} = await api.signUp(formData);
-
+    console.log(data)
     dispatch({type: AUTH, data});
 
     history.push("/");
